90240a4b77d1c40a6e10697add7f809e707678a9
Ezra-Ben <ezrabunks@gmail.com>
2025-06-18 08:25:37 +0300

    Update Vendor.php

--- app/Models/Vendor.php
+++ app/Models/Vendor.php
@@ -8,20 +8,10 @@ class Vendor extends Model
 {
 protected $fillable = [
     'name',
+    'business_name', 
     'registration_number',
-    'email',
-    'phone',
-    'address',
-    'previous_clients',       // JSON
-    'transaction_history',    // JSON
-    'industry_ratings',       // Float
-    'product_categories',     // JSON
-    'material_types',         // JSON
-    'pricing_range',
-    'bulk_availability',      // Boolean
-    'certifications',         // JSON
-    'business_license',
-    'tax_identification',
-    // Add other fields you want to be mass-assignable
+    'contact',   
+    'product_category',    
+    'business_license_url',
 ];
 }
