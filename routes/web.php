7afa5f15be342d94a77b1039b97540155cfdad25
Kristiana-Sengonzi <kristianasengonzi@gmail.com>
2025-06-19 12:45:18 +0300

    Revert "Updated web.php"
    This reverts commit 6c40e8569bace0e96a9ef0f93c20a50d92091756.

--- routes/web.php
+++ routes/web.php
@@ -13,21 +13,6 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-Route::prefix('settings')->name('settings.')->middleware(['auth'])->group(function () {
-    Route::get('/profile', function () {
-        return view('livewire.settings.profile');
-    })->name('profile');
-
-    Route::get('/password', function () {
-        return view('livewire.settings.password');
-    })->name('password');
-
-    Route::get('/appearance', function () {
-        return view('livewire.settings.appearance');
-    })->name('appearance');
-});
-
-
 Route::get('vendor/register', [VendorController::class, 'showForm'])->name('vendor.form');
 Route::post('vendor/register', [VendorController::class, 'submitForm'])->name('vendor.register');
 
