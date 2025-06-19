bb2b058dfa280c439e4697e2ae5471e917536337
Kristiana-Sengonzi <kristianasengonzi@gmail.com>
2025-06-19 12:48:54 +0300

    Revert "Removed BootStrap CSS & JS Links"
    This reverts commit 82728229358766cf6f9cfbebfd4e5102724a3ad0.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,8 @@
         
         <link rel="stylesheet" href="{{ asset('css/style.css') }}">
         
+        <!-- Bootstrap 5.3.3 CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
       
 
         <!-- Scripts -->
@@ -36,6 +38,8 @@
                 {{ $slot }}
             </main>
         </div>
+    <!-- Bootstrap 5.3.3 JS Bundle (includes Popper) -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
 
     </body>
 </html>
