a25bd2102378338e6d94259cdbcd15325c310c83
Ezra-Ben <ezrabunks@gmail.com>
2025-07-14 18:48:51 +0300

    Disabled tests

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (config('app.name') === 'J-Clothes' || trim($slot) === 'J-Clothes')
-<img src="{{ url('images/j-clothes-logo.png') }}" class="logo" alt="J-Clothes Logo" style="height: 75px; width: auto;">
+<img src="https://raw.githubusercontent.com/Ezra-Ben/clothe-scm/main/public/images/j-clothes-logo.png" class="logo" alt="J-Clothes Logo" style="height: 75px; width: auto;">
 @elseif (trim($slot) === 'Laravel')
 <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @else
