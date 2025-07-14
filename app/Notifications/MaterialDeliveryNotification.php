0f9e77a553a8e8a2d5cd7de664d89843bad7483d
Ezra-Ben <ezrabunks@gmail.com>
2025-07-14 09:48:08 +0300

    Supplier Delivery notifications

--- app/Notifications/MaterialDeliveryNotification.php
+++ app/Notifications/MaterialDeliveryNotification.php
@@ -2,21 +2,24 @@
 
 namespace App\Notifications;
 
+use App\Models\ProcurementReply;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class MaterialDeliveryNotification extends Notification
+class MaterialDeliveryNotification extends Notification implements ShouldQueue
 {
     use Queueable;
+
+    protected $reply;
 
     /**
      * Create a new notification instance.
      */
-    public function __construct()
+    public function __construct(ProcurementReply $reply)
     {
-        //
+        $this->reply = $reply;
     }
 
     /**
@@ -26,7 +29,7 @@ class MaterialDeliveryNotification extends Notification
      */
     public function via(object $notifiable): array
     {
-        return ['mail'];
+        return ['mail', 'database'];
     }
 
     /**
@@ -35,9 +38,15 @@ class MaterialDeliveryNotification extends Notification
     public function toMail(object $notifiable): MailMessage
     {
         return (new MailMessage)
-            ->line('The introduction to the notification.')
-            ->action('Notification Action', url('/'))
-            ->line('Thank you for using our application!');
+            ->subject('Material Delivery Incoming - Validation Required')
+            ->greeting('Hello ' . $notifiable->name . ',')
+            ->line('A supplier has marked materials as shipped and they are incoming for delivery.')
+            ->line('**Material:** ' . $this->reply->request->rawMaterial->name)
+            ->line('**Supplier:** ' . $this->reply->supplier->vendor->name)
+            ->line('**Quantity:** ' . $this->reply->quantity_confirmed . ' units')
+            ->line('**Expected Delivery:** ' . $this->reply->expected_delivery_date)
+            ->action('Validate Delivery', url(route('procurement.replies.indexForRequest', $this->reply->request->id)))
+            ->line('Please validate the delivery once materials arrive to update inventory.');
     }
 
     /**
@@ -48,7 +57,13 @@ class MaterialDeliveryNotification extends Notification
     public function toArray(object $notifiable): array
     {
         return [
-            //
+            'message' => 'Material delivery incoming from ' . $this->reply->supplier->vendor->name,
+            'material_name' => $this->reply->request->rawMaterial->name,
+            'supplier_name' => $this->reply->supplier->vendor->name,
+            'quantity' => $this->reply->quantity_confirmed,
+            'expected_date' => $this->reply->expected_delivery_date,
+            'reply_id' => $this->reply->id,
+            'request_id' => $this->reply->request->id,
         ];
     }
 }
